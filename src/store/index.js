9fd71a1c6cab3f112a546ad26ac3bc7fa8c2dd05
xus <754124808@qq.com>
2018-10-15 15:09:59 +0800

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default store = new Vuex.Store({
+  modules: {
+    app,
+    user
+  },
+  getters
+})
---
+++ src/views/layout/layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'layout'
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.layout{
+  width: 1200px;
+  margin: 0 auto;
+  min-height: 100%;
+  background-color: black;
+}
+</style>
